f206acd19bd6a17e8b9c53460c6b6d6200d6cf24
litarahmasadina <litarahmasadina@gmail.com>
2026-04-05 00:16:31 +0700

    Add files via upload

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,25 +3,53 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-# 1. Menyiapkan Data
+st.set_page_config(layout="wide")
+
+# Menyiapkan Data
 day_df = pd.read_csv("dashboard/main_data.csv")
 hour_df = pd.read_csv("dashboard/hour.csv")
 
 st.header('Bike Sharing Dashboard')
 
-# Visualisasi 1: Musim 
-st.subheader('Rata-rata Penyewaan Sepeda Berdasarkan Musim')
-fig1, ax1 = plt.subplots(figsize=(10, 5))
-colors_season = ["#D3D3D3", "#D3D3D3", "#005b96", "#D3D3D3"]
-sns.barplot(x='season', y='cnt', data=day_df, palette=colors_season, ax=ax1)
-st.pyplot(fig1)
+# Visualisasi 1 & 2 (Dibuat Berdampingan)
+col1, col2 = st.columns(2)
 
-# Visualisasi 2: Jam Sibuk
-st.subheader('Tren Jam Sibuk: Hari Kerja vs Hari Libur')
-fig2, ax2 = plt.subplots(figsize=(10, 5))
-sns.pointplot(x='hr', y='cnt', hue='workingday', data=hour_df, palette='Set1', ax=ax2)
-ax2.set_xlabel('Jam (0-23)')
-ax2.set_ylabel('Rata-rata Penyewaan')
-st.pyplot(fig2)
+with col1:
+    st.subheader('Rata-rata Penyewaan Berdasarkan Musim')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    colors_season = ["#D3D3D3", "#D3D3D3", "#005b96", "#D3D3D3"]
+    sns.barplot(x='season', y='cnt', data=day_df, palette=colors_season, ax=ax)
+    ax.set_xlabel('Musim (1: Springer, 2: Summer, 3: Fall, 4: Winter)')
+    ax.set_ylabel('Rata-rata Penyewaan')
+    st.pyplot(fig)
+
+with col2:
+    st.subheader('Rata-rata Penyewaan Berdasarkan Kondisi Cuaca')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    colors_weather = ["#005b96", "#D3D3D3", "#D3D3D3"]
+    sns.barplot(x='weathersit', y='cnt', data=day_df, palette=colors_weather, ax=ax)
+    ax.set_xlabel('Kondisi Cuaca (1: Cerah, 2: Mendung, 3: Hujan/Salju Ringan)')
+    ax.set_ylabel('Rata-rata Penyewaan')
+    st.pyplot(fig)
+
+# Visualisasi 3: Jam Sibuk
+st.subheader('Tren Jam Sibuk Penyewaan Sepeda: Hari Kerja vs Hari Libur')
+fig, ax = plt.subplots(figsize=(12, 6))
+
+# Ubah angka 0 & 1 jadi teks biar legendanya bagus
+hour_df['Keterangan'] = hour_df['workingday'].map({0: 'Hari Libur', 1: 'Hari Kerja'})
+
+sns.pointplot(
+    x='hr', 
+    y='cnt', 
+    hue='Keterangan', 
+    data=hour_df, 
+    palette={'Hari Libur': '#ff8c8c', 'Hari Kerja': '#5dade2'}, 
+    ax=ax
+)
+ax.set_xlabel('Jam (00.00 - 23.00)')
+ax.set_ylabel('Rata-rata Penyewaan')
+ax.grid(True, linestyle='--', alpha=0.6)
+st.pyplot(fig)
 
 st.caption('Copyright (c) Lita Rahma Sadina 2026')
